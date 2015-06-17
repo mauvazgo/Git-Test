d1b9f7304936288561cf4d29d846cbd6a3f7f2e8
Mauricio <mvazquez@ayesa.com>
2015-06-17 09:08:08 +0200

    soluciona incidencia #3

--- Proyecto1/src/main/java/com/ayesa/redmine/RedmineTest2.java
+++ Proyecto1/src/main/java/com/ayesa/redmine/RedmineTest2.java
@@ -6,21 +6,21 @@ import com.taskadapter.redmineapi.RedmineManager;
 import com.taskadapter.redmineapi.bean.User;
 
 public class RedmineTest2 {
-	
+
 	private static String uri = "http://127.0.0.1:81/redmine";
 	private static String apiAccessKey = "WpXFreqOrHqIJ0tpzw8J";
 	private static String projectKey = "prueba1";
 	private static Integer queryId = null;
-			
-	public static void main (String[] args) {
-	
+
+	public static void main(String[] args) {
+
 		try {
-			
+
 			RedmineManager mgr = new RedmineManager(uri, apiAccessKey);
-			List<User> users = (List<User>)mgr.getUsers();
+			List<User> users = (List<User>) mgr.getUsers();
 			for (User user : users) {
-		        System.out.println(user.toString());
-		    }
+				System.out.println(user.toString());
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
