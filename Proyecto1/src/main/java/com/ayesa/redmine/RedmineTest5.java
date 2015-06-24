de558a8b420630a426bf14e5e2df735eb10f38c6
Mauricio <mvazquez@ayesa.com>
2015-06-24 11:14:54 +0200

    arreglos tareas #4 y #5

---
+++ Proyecto1/src/main/java/com/ayesa/redmine/RedmineTest5.java
@@ -0,0 +1,31 @@
+package com.ayesa.redmine;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import com.taskadapter.redmineapi.RedmineManager;
+import com.taskadapter.redmineapi.RedmineManagerFactory;
+import com.taskadapter.redmineapi.bean.User;
+
+public class RedmineTest5 {
+
+	private final static String uri = "http://127.0.0.1:81/redmine";
+	private final static String apiAccessKey = "WpXFreqOrHqIJ0tpzw8J";
+
+	public static void main(String[] args) throws Exception {
+
+		try {
+
+			RedmineManager mgr = RedmineManagerFactory.createWithApiKey(uri,
+					apiAccessKey);
+			List<User> users = (List<User>) mgr.getUserManager().getUsers();
+			for (User user : users) {
+				System.out.println(user.toString());
+			}
+		} catch (Exception e) {
+			Logger.getGlobal().log(null, " msg");
+			throw e;
+
+		}
+	}
+}
